153cc1c7bd799926e970ceba7a311d1fb893c55a
Matias Mariani <pm_mariani@hotmail.com>
2018-04-30 14:23:54 -0300

    add ArduinoJson library and message parser

--- fire_alarm/fire_alarm.ino
+++ fire_alarm/fire_alarm.ino
@@ -1,8 +1,42 @@
 #include "Arduino.h"
+#include "WString.h"
+#include "ArduinoJson.h"
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+
+	Serial.begin(9600);
+	char order[] = "{\"action\":\"CONNECT\",\"ssid\": \"red\",\"password\":\"contrasena\" }";
+
+	DynamicJsonBuffer jsonBuffer(512);
+
+	JsonObject& root = jsonBuffer.parseObject(order);
+
+
+	String action = root["action"];
+	String ssid = root["ssid"];
+	String password = root["password"];
+	Serial.println(ssid);
+	Serial.println(action);
+	Serial.println(password);
+
+	char locationOrder[] = "{ \"action\":\"SET_LOCATION\", \"latitude\": { \"degree\": 33 ,\"minute\":10, \"second\":45.2345, \"cardinal_point\":\"SOUTH\"}}";
+	JsonObject& location  = jsonBuffer.parseObject(locationOrder);
+
+	String action2 = location["action"];
+
+	int degree = location["latitude"]["degree"];
+	int minute = location["latitude"]["minute"];
+	double second = location["latitude"]["second"];
+	String cardinalPoint = location["latitude"]["cardinal_point"];
+
+	Serial.println(action2);
+	Serial.println(degree);
+	Serial.println(minute);
+	Serial.println(second);
+	Serial.println(cardinalPoint);
+
 }
 
 // The loop function is called in an endless loop
--- fire_alarm/sloeber.ino.cpp
+++ fire_alarm/sloeber.ino.cpp
@@ -2,10 +2,12 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-04-30 11:11:58
+//This file has been generated on 2018-04-30 14:21:35
 
 #include "Arduino.h"
 #include "Arduino.h"
+#include "WString.h"
+#include "ArduinoJson.h"
 
 void setup() ;
 void loop() ;
